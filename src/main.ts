5769e7dbf17aec74d1016456a1b4167899d18c30
Saurabh Gupta <saurabhgupta0508@gmail.com>
2024-01-21 03:00:54 +0530

    theme fix

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,27 @@ const inputTextArea = document.getElementById(
 ) as HTMLTextAreaElement;
 const parseButton = document.getElementById("process-btn") as HTMLButtonElement;
 const parsedOutputContainer = document.getElementById("parsed-data");
-const copyButton = document.getElementById("copy-btn");
+const copyButton = document.getElementById("copy-btn") as HTMLButtonElement;
 const tooltip = document.getElementById("copy-tooltip");
 const copyIcon = document.getElementById("copy-icon");
 const copiedIcon = document.getElementById("copied-icon");
+const lightModeInput = document.getElementById(
+  "light-mode"
+) as HTMLInputElement;
+const darkModeInput = document.getElementById("dark-mode") as HTMLInputElement;
+
+// Init theme
+// By default theme is picked from system settings (prefers-color-scheme)
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  lightModeInput.checked = savedTheme === "light";
+  darkModeInput.checked = savedTheme === "dark";
+} else {
+  // If no theme is saved then set the correct toggle based on system settings
+  const lightThemeMq = window.matchMedia("(prefers-color-scheme: light)");
+  lightModeInput.checked = lightThemeMq.matches;
+  darkModeInput.checked = !lightThemeMq.matches;
+}
 
 const parseSMS = () => {
   const sms = (inputTextArea.value ?? "").trim();
@@ -41,3 +58,12 @@ const copyParsedData = () => {
 
 parseButton.addEventListener("click", parseSMS);
 copyButton?.addEventListener("click", copyParsedData);
+lightModeInput.addEventListener("change", () => {
+  if (lightModeInput.checked) {
+    localStorage.setItem("theme", "light");
+    darkModeInput.checked = false;
+  } else {
+    localStorage.setItem("theme", "dark");
+    darkModeInput.checked = true;
+  }
+});
